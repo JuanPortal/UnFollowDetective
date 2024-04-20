eacc0e0b36118e782a637e23600688537764693a
Portal <juanportalg@gmail.com>
2024-04-19 17:08:41 -0700

    basics

--- src/UnFollowDetective.jsx
+++ src/UnFollowDetective.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const UnFollowDetective = () => {
   return (
-    <h1>UnFollowDetective</h1>
+    <>
+      <h1>unFollowDetective</h1>
+
+      <button onClick={()=>console.log('clicked!')}>Log in with Instagram</button>
+    </>
   )
 }
